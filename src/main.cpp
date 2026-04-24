793b56ef46a601b02eb8465bd60affe2aef6e18a
carolinaprian <a25110288@ceti.mx>
2026-04-24 01:10:58 +0000

    Práctica 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <Foco.hpp>
 
+using namespace std;
 
 int main (int argc, char* argv[])
 {
-    while (/*condition*/)
-    {
-        /* code */
-    }
-    
-    return EXIT_SUCCESS;
+    Foco foquito;
+    foquito.Encender();
+    foquito.Apagar();
+ 
+    cout << "Estado Foco: " << 
+        foquito.LeerEstado() << endl;
+
+    return 0;
 }
